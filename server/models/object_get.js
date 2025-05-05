94476424679475a6c3647c454388d652a6d00354
apponio <apponio0210@naver.com>
2025-05-05 15:47:42 +0900

    feat : 분실물 제보(습득) 게시판 기본 기능 구현현

---
+++ server/models/object_get.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// 분실물 습득 게시판 (Object_get)
+const object_get_schema = mongoose.Schema({
+    id: { type: String },
+    user_id: { type: String },
+    date: { type: Date },
+    atcId: { type: String },
+    fdPrdtNm: { type: String },
+    fdYmd: { type: String },
+    fdHor: { type: String },
+    fdPlace: { type: String },
+    uniq: { type: String },
+    fdSn: { type: String },
+    prdtClNm: { type: String },
+    depPlace: { type: String },
+    csteSteNm: { type: String },
+    orgId: { type: String },
+    orgNm: { type: String },
+    tel: { type: String },
+    fndKeepOrgnSeNm: { type: String },
+    fdFilePathImg: { type: String }
+}, { versionKey: false });
+
+const Object_get = mongoose.model('Object_get', object_get_schema, 'object_get');
+
+module.exports = { Object_get };
