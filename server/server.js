847dd7cdbef21493d7f02746114e6e85fcfad070
LeeJH <kinglee4290@gmail.com>
2025-05-09 13:19:35 +0900

    feat: animal 라우터 추가가

--- server/server.js
+++ server/server.js
@@ -8,12 +8,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'views')));
-
 mongoose.connect(process.env.DB_URL)
 .then( ()=>console.log('몽고DB Connected...'))
 .catch(err=>console.log('몽고디비 에러',err))
 
 app.use('/', require('./routes/user.js'))
+app.use('/animal', require('./routes/animal.js')) // 요청 주소가 /animal/* 일 때 이 라우터로 이동
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
