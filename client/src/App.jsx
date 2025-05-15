b8d43db468864c370d224ad7ff3d1f0d0a26699b
추동훈 <cdh5113@naver.com>
2025-05-15 21:11:43 +0900

    feat:loginpage추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,9 @@ import Object from './pages/Object';
 import Animal from './pages/Animal';
 import Reward from './pages/Reward';
 import Notfound from './pages/Notfound';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 import { getIconImage } from './util/get-img-icon';
 import Button from './components/Button';
 import Header from './components/Header';
@@ -12,17 +15,17 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header 
+      {/* <Header 
         icon={<img src={getIconImage(1)} style={{width: '150px', height: 'auto'}} />}
         mainTitle={"찾Go"} 
         subTitle={"Find Lost Items"}
         mypage={<Button text={"마이페이지"}/>}
         login={<Button text={"로그인"} />}
         register={<Button text={"회원가입"} type={"BLACK"} />}
-      />
+      /> */}
 
-      <Button text={"123"} type={"DEFAULT"}/>
-      <Button text={"123"} type={"BLACK"}/>
+      {/* <Button text={"123"} type={"DEFAULT"}/>
+      <Button text={"123"} type={"BLACK"}/> */}
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -30,6 +33,8 @@ function App() {
         <Route path="/animal" element={<Animal />} />
         <Route path="/reward/:id" element={<Reward />} />
         <Route path="*" element={<Notfound />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </>
     
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import LoginElement from "../components/LoginElement";
+
+const Login = ()=>{
+  return(
+    <div>
+      <LoginElement />
+    </div>
+  )
+};
+
+export default Login;
+
