03f2de3967ad60f843ea2101d79b6c8f639a675e
apponio <apponio0210@naver.com>
2025-05-22 17:04:24 +0900

    feat: 분실물 습득 제보 끝끝

--- server/server.js
+++ server/server.js
@@ -24,22 +24,22 @@ app.use(methodOverride('_method'));
 
 app.use(passport.initialize())
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        maxAge: 60 * 60 * 1000
-    },
-    store: MongoStore.create({
-        mongoUrl: process.env.DB_URL,
-        dbName: process.env.DB_NAME,
-    })
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 60 * 60 * 1000
+  },
+  store: MongoStore.create({
+    mongoUrl: process.env.DB_URL,
+    dbName: process.env.DB_NAME,
+  })
 }))
 app.use(passport.session())
 
 mongoose.connect(process.env.DB_URL)
-    .then(() => console.log('몽고DB Connected...'))
-    .catch(err => console.log('몽고디비 에러', err))
+  .then(() => console.log('몽고DB Connected...'))
+  .catch(err => console.log('몽고디비 에러', err))
 
 const swaggerOptions = {
   definition: {
@@ -71,11 +71,12 @@ app.use('/api/object/get', require('./routes/object_get.js'))
 app.use('/api/object/lost', require('./routes/object_lost.js'))
 app.use('/api/reward/animal', require('./routes/reward_animal.js'))
 app.use('/api/reward/object', require('./routes/reward_object.js'))
+app.use('/api/images', express.static(path.join(__dirname, './images')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
 
 app.listen(PORT, () => {
-    console.log(`server running on PORT ${PORT}`);
+  console.log(`server running on PORT ${PORT}`);
 })
